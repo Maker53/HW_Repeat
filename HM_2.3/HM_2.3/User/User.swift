9ea0ac7e1fa0207f7a686d5071a85df9a4b31ef2
Shalgin Stanislav <s.shalgin@gmail.com>
2021-12-14 20:56:32 +0300

    Create model (User)

---
+++ HM_2.3/HM_2.3/User/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  HM_2.3
+//
+//  Created by Станислав on 14.12.2021.
+//
+
+struct User {
+    let name = "User"
+    let password = "123"
+    let person: Person
+}
+
+struct Person {
+    let image = "singletone"
+}
