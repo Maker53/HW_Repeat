5b59d3572d6dad7aa84158e6d7913e8e0bce043d
Shalgin Stanislav <s.shalgin@gmail.com>
2022-01-02 18:38:18 +0300

    VC code done

--- HW_2.9/HW_2.9/View Controllers/ViewController.swift
+++ HW_2.9/HW_2.9/View Controllers/ViewController.swift
@@ -5,33 +5,54 @@
 //  Created by Станислав on 02.01.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
-    @IBOutlet var animationView: UIView!
+    @IBOutlet var animationView: SpringView!
     @IBOutlet var animationLabel: UILabel!
     
-    let animation = Animation.getAnimation()
+    var animation = Animation.getAnimation()
+    
+    private var firstAnimation = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        animationLabel.text = """
-Preset: \(animation.animation)
-Curve: \(animation.curve)
-Force: \(toString(animation.force))
-Duration: \(toString(animation.duration))
-Delay: \(toString(animation.delay))
-"""
+       setTitle(for: animationLabel)
     }
 
     @IBAction func runAnimation(_ sender: UIButton) {
+        if !firstAnimation {
+            setTitle(for: animationLabel)
+        } else {
+            firstAnimation.toggle()
+        }
+        
+        animationView.animation = animation.animation
+        animationView.curve = animation.curve
+        animationView.force = animation.force
+        animationView.duration = animation.duration
+        animationView.delay = animation.delay
+        animationView.animate()
+        
+        animation = Animation.getAnimation()
+        sender.setTitle("Run \(animation.animation)", for: .normal)
     }
 }
 
 extension ViewController {
     private func toString(_ double: Double) -> String {
         String(format: "%.2f", double)
+    }
+    
+    private func setTitle(for label: UILabel) {
+        label.text = """
+Preset: \(animation.animation)
+Curve: \(animation.curve)
+Force: \(toString(animation.force))
+Duration: \(toString(animation.duration))
+Delay: \(toString(animation.delay))
+"""
     }
 }
