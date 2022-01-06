9424486e48e7c78d82f45a0065fd63488b17e3ff
Shalgin Stanislav <s.shalgin@gmail.com>
2022-01-06 15:02:43 +0300

    Add API link to model

--- HW_2.10/HW_2.10/Models/RandomDogData.swift
+++ HW_2.10/HW_2.10/Models/RandomDogData.swift
@@ -1,10 +1,14 @@
 //
-//  RandomDogData.swift
+//  RandomDog.swift
 //  HW_2.10
 //
 //  Created by Станислав on 05.01.2022.
 //
 
-struct RandomDogData: Decodable {
+struct RandomDog: Decodable {
     let message: String
+}
+
+enum Link: String {
+    case randomDogApi = "https://dog.ceo/api/breeds/image/random"
 }
