ab4c133d83fa14ad4b5e3ee19894c25cb88d545a
Shalgin Stanislav <s.shalgin@gmail.com>
2022-01-06 15:01:39 +0300

    Rebuild Network Manager

---
+++ HW_2.10/HW_2.10/Services/NetworkManager.swift
@@ -0,0 +1,45 @@
+//
+//  NetworkManager.swift
+//  HW_2.10
+//
+//  Created by Станислав on 05.01.2022.
+//
+
+import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchData(from url: String, with completion: @escaping (RandomDog) -> Void) {
+        guard let url = URL(string: url) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let randomDog = try JSONDecoder().decode(RandomDog.self, from: data)
+                DispatchQueue.main.async {
+                    completion(randomDog)
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+}
+
+class ImageManager {
+    static let shared = ImageManager()
+    
+    private init() {}
+    
+    func fetchImage(from url: String) -> Data? {
+        guard let url = URL(string: url) else { return nil }
+        return try? Data(contentsOf: url)
+    }
+}
