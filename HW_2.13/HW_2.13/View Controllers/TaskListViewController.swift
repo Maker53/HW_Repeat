721d9e0dba5a67ac5ed51a6a92bce8299066ead6
Shalgin Stanislav <s.shalgin@gmail.com>
2022-01-23 10:07:41 +0300

    Add storyboard and rebuild ViewController to TableVC

---
+++ HW_2.13/HW_2.13/View Controllers/TaskListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TaskListViewController.swift
+//  HW_2.13
+//
+//  Created by Станислав on 23.01.2022.
+//
+
+import UIKit
+
+class TaskListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        title = "Task List"
+    }
+
+
+}
+
