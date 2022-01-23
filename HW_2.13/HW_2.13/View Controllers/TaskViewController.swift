8bd5498a92522fd8f6d86f4cd4b1f5f35196e498
Shalgin Stanislav <s.shalgin@gmail.com>
2022-01-23 10:49:05 +0300

    Add TaskVC segue

---
+++ HW_2.13/HW_2.13/View Controllers/TaskViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TaskViewController.swift
+//  HW_2.13
+//
+//  Created by Станислав on 23.01.2022.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+}
