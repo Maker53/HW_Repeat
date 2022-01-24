d4df193c6bc19048e752d1919f3c40212405c144
Shalgin Stanislav <s.shalgin@gmail.com>
2022-01-24 20:30:58 +0300

    feat(DataManager): add Data Manager for temp tasks and taskLists

---
+++ HW_2.14/HW_2.14/Services/DataManager.swift
@@ -0,0 +1,16 @@
+//
+//  DataManager.swift
+//  HW_2.14
+//
+//  Created by Станислав on 24.01.2022.
+//
+
+class DataManager {
+    static let shared = DataManager()
+    
+    private init() {}
+    
+    func createTempData(completion: @escaping () -> Void) {
+        
+    }
+}
