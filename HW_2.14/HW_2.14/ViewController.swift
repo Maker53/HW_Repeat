e235bdb3fb084e36bb42ba4fbf6bce72b7ceb81e
Shalgin Stanislav <s.shalgin@gmail.com>
2022-01-24 07:53:51 +0300

    Initial commmit

---
+++ HW_2.14/HW_2.14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2.14
+//
+//  Created by Станислав on 24.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
