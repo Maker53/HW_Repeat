644877bfe71457027cce9ef37fe807ec0ce3ff6a
Shalgin Stanislav <s.shalgin@gmail.com>
2022-01-24 20:28:25 +0300

    feat(StorageManager): add Storage Manager

---
+++ HW_2.14/HW_2.14/Services/StorageManager.swift
@@ -0,0 +1,38 @@
+//
+//  StorageManager.swift
+//  HW_2.14
+//
+//  Created by Станислав on 24.01.2022.
+//
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    private init() {}
+    
+    // MARK: - Task List
+    func save(_ taskList: TaskList) {
+        
+    }
+    
+    func save(_ taskList: [TaskList]) {
+        
+    }
+    
+    func delete(_ taskList: TaskList) {
+        
+    }
+    
+    func edit(_ taskList: TaskList, newValue: String) {
+        
+    }
+    
+    func done(_ taskList: TaskList) {
+        
+    }
+    
+    // MARK: - Tasks
+    func save(_ task: Task, to taskList: TaskList) {
+        
+    }
+}
